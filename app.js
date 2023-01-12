cf3a091857d189906962978474167bed6fe493c0
VishalSharma-123 <b20239@students.iitmandi.ac.in>
2023-01-12 16:22:52 +0530

    Learned CRUD Operations in Express and Node. Testing through Postman.

---
+++ app.js
@@ -0,0 +1,98 @@
+const express = require("express");
+const fs = require("fs");
+const app = express();
+
+app.use(express.json()); //Middleware
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get("/api/v1/tours", (req, res) => {
+  res.status(200).json({
+    status: "Success",
+    results: tours.length,
+    data: {
+      tours: tours,
+    },
+  });
+});
+
+app.get("/api/v1/tours/:id", (req, res) => {
+  const id = req.params.id * 1;
+
+  if (id >= tours.length) {
+    return res.status(404).json({
+      status: "Error",
+      message: "Invalid tour id",
+    });
+  }
+
+  const tour = tours.find((el) => el.id == id);
+
+  res.status(200).json({
+    status: "Success",
+    data: {
+      tour: tour,
+    },
+  });
+});
+
+app.post("/api/v1/tours", (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: "Success",
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+});
+
+app.patch("/api/v1/tours/:id", (req, res) => {
+  const id = req.params.id * 1;
+
+  if (id >= tours.length) {
+    return res.status(404).json({
+      status: "Error",
+      message: "Invalid tour id",
+    });
+  }
+
+  res.status(200).json({
+    status: "Success",
+    data: {
+      tour: "Updated tours object successfully",
+    },
+  });
+});
+
+app.delete("/api/v1/tours/:id", (req, res) => {
+  const id = req.params.id * 1;
+
+  if (id >= tours.length) {
+    return res.status(404).json({
+      status: "Error",
+      message: "Invalid tour id",
+    });
+  }
+
+  res.status(204).json({
+    status: "Success",
+    data: null,
+  });
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log("App listening on port: 3000");
+});
