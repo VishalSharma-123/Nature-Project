8ff4bd2533c30a9693a6f6ea7452ac9c10bb4ba0
VishalSharma-123 <b20239@students.iitmandi.ac.in>
2023-01-13 21:44:00 +0530

    Added the controllers and routes to a separate file. Used Route Mounting. Also add server.js file.

---
+++ controllers/userController.js
@@ -0,0 +1,34 @@
+exports.getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: "Error",
+    message: "This route is not yet created",
+  });
+};
+
+exports.createUser = (req, res) => {
+  res.status(500).json({
+    status: "Error",
+    message: "This route is not yet created",
+  });
+};
+
+exports.getUser = (req, res) => {
+  res.status(500).json({
+    status: "Error",
+    message: "This route is not yet created",
+  });
+};
+
+exports.updateUser = (req, res) => {
+  res.status(500).json({
+    status: "Error",
+    message: "This route is not yet created",
+  });
+};
+
+exports.deleteUser = (req, res) => {
+  res.status(500).json({
+    status: "Error",
+    message: "This route is not yet created",
+  });
+};
