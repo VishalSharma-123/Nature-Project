c81460f421ac91abb4342fb3ef867b28644435bc
VishalSharma-123 <b20239@students.iitmandi.ac.in>
2023-10-30 02:18:28 +0530

    gitignore

--- server.js
+++ server.js
@@ -1,9 +1,53 @@
-const dotenv = require("dotenv");
+const dotenv = require('dotenv');
 dotenv.config({ path: `${__dirname}/config.env` });
 const app = require(`${__dirname}/app.js`);
+const mongoose = require('mongoose');
 
-// creating the server
+const DATABASE = process.env.DB.replace('<PASSWORD>', process.env.DB_PASSWORD);
 const port = process.env.PORT;
-app.listen(port, () => {
-  console.log(`Listening at port ${port}.`);
+
+mongoose
+  .connect(DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+
+  .then(() => {
+    console.log('DB connection successful!');
+    app.listen(port, () => {
+      console.log(`Listening at port ${port}.`);
+    });
+  });
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
 });
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The Forest Hiker',
+  rating: 4.7,
+  price: 397,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log('Error: ', err);
+  });
+// creating the server
